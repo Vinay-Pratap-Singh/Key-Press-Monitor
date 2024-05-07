93d49cfbdc915e8b3930ab26473db70a5941ba0d
Vinay Pratap Singh <technicalharvi@gmail.com>
2024-05-07 07:27:47 +0530

    feat: displaying the clicked key on UI

--- script/keypress/script.js
+++ script/keypress/script.js
@@ -1,6 +1,24 @@
+const keyboardButtonContainer = document.createElement("ul");
+keyboardButtonContainer.setAttribute("id", "keyboardButtonContainer");
+document.body.prepend(keyboardButtonContainer);
+
+const buttonList = [];
+
+const renderKeys = () => {
+  keyboardButtonContainer.innerHTML = "";
+  buttonList.forEach((button) => {
+    keyboardButtonContainer.appendChild(button);
+  });
+};
+
 document.addEventListener("keydown", (event) => {
-  const heading = document.createElement("h1");
-  heading.classList.add("newTestItem");
-  heading.innerText = event.key;
-  document.body.prepend(heading);
+  const listItem = document.createElement("li");
+  listItem.classList.add("keyboardButton");
+  listItem.innerText = event.key;
+  buttonList.push(listItem);
+  renderKeys();
+  setTimeout(() => {
+    buttonList.shift();
+    renderKeys();
+  }, 2500);
 });
