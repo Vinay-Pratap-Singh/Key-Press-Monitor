1b81b6618522163e680ffcda687af939f031f04d
Vinay Pratap Singh <technicalharvi@gmail.com>
2024-05-12 19:11:05 +0530

    fix: working toggle to turn on or off the keypress event

--- background.js
+++ background.js
@@ -1,22 +1,14 @@
+const doesScriptInjected = {};
+
 chrome.runtime.onMessage.addListener(function (message) {
   const { toggleState, tabId } = message;
+
   if (!tabId) return;
-  if (toggleState && tabId) {
+  if (toggleState && !doesScriptInjected[tabId]) {
     chrome.scripting.executeScript({
       target: { tabId },
       files: ["content.js"],
     });
-  } else {
-    chrome.scripting.executeScript({
-      target: { tabId },
-      function: () => {
-        const keyboardButtonContainer = document.getElementById(
-          "keyboardButtonContainer"
-        );
-        if (keyboardButtonContainer) {
-          keyboardButtonContainer.remove();
-        }
-      },
-    });
+    doesScriptInjected[tabId] = true;
   }
 });
--- content.js
+++ content.js
@@ -1,17 +1,18 @@
-const keyboardButtonContainer = document.createElement("ul");
-keyboardButtonContainer.setAttribute("id", "keyboardButtonContainer");
-document.body.prepend(keyboardButtonContainer);
-
+let keyboardButtonContainer = null;
 const buttonList = [];
 
-const renderKeys = () => {
+// for first time
+handleToggleTrueState();
+
+function renderKeys() {
+  if (!keyboardButtonContainer) return;
   keyboardButtonContainer.innerHTML = "";
   buttonList.forEach((button) => {
     keyboardButtonContainer.appendChild(button);
   });
-};
+}
 
-document.addEventListener("keydown", (event) => {
+function handleKeyPress(event) {
   const listItem = document.createElement("li");
   listItem.classList.add("keyboardButton");
   listItem.innerText = event.key;
@@ -21,4 +22,30 @@ document.addEventListener("keydown", (event) => {
     buttonList.shift();
     renderKeys();
   }, 2500);
+}
+
+function handleToggleTrueState() {
+  keyboardButtonContainer = document.createElement("ul");
+  keyboardButtonContainer.setAttribute("id", "keyboardButtonContainer");
+  document.body.prepend(keyboardButtonContainer);
+  document.addEventListener("keydown", handleKeyPress);
+}
+
+function handleToggleFalseState() {
+  const keyboardButtonContainer =
+    document.getElementById("keyboardButtonContainer") || null;
+  if (keyboardButtonContainer) {
+    keyboardButtonContainer.remove();
+  }
+  buttonList.length = 0;
+  document.removeEventListener("keydown", handleKeyPress);
+}
+
+// Listen for changes to toggleState
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  if (namespace === "sync" && "toggleState" in changes) {
+    const newToggleState = changes.toggleState.newValue;
+    console.log("Toggle state changed:", newToggleState);
+    newToggleState ? handleToggleTrueState() : handleToggleFalseState();
+  }
 });
