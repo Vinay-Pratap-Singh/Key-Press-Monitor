93a2724ebac1c0d9bf7d4fc8c0727b1ca09c9f35
Vinay Pratap Singh <technicalharvi@gmail.com>
2024-05-13 20:48:29 +0530

    fix: toggle state default value on page load

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const toggle = document.getElementById("toggle");
 let tabId;
+let initialized = false;
 
 // getting current tabID
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   const currentTab = tabs[0];
   tabId = currentTab.id;
-});
 
-// get toggle state from storage
-chrome.storage.sync.get("toggleState", function (data) {
-  toggle.checked = data.toggleState || false;
+  // Set toggle state from storage
+  chrome.storage.sync.get("toggleState", function (data) {
+    toggle.checked = data.toggleState || false;
+  });
+
+  // Set initialized to true after the first load
+  initialized = true;
 });
 
 toggle.addEventListener("change", function () {
   const toggleState = this.checked;
   chrome.storage.sync.set({ toggleState });
   chrome.runtime.sendMessage({ toggleState, tabId });
+});
+
+// Listen for changes to storage
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  if (namespace === "sync" && initialized) {
+    const newToggleState = changes.toggleState.newValue;
+    toggle.checked = newToggleState;
+  }
 });
